6d34e2f19ed2519cd3126e8ac44f26742628eec4
Yvaine Liew <yvaine8888@gmail.com>
2024-11-15 04:51:00 +0000

    I created everything else but is having trouble with the dict.

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 import dockingBays as db
 import itertools
+from starterProject import *
+from collections import OrderedDict
 # Note: I used W3Schools to figure out append, end in print statement, and remove.
-# I also found out about itertools from GeeksforGeeks.
-
-# Function to print docking bays information
-def print_docking_bays():
-    print("Docking Bays")
-    for bay in db.docking_bays:
-        print(f"Bay {bay['bay_id']} - Size: {bay['size']}, Schedule: {bay['schedule']}")
-
-# Function to print incoming ships information
-def print_incoming_ships():
-    print("\nIncoming Ships")
-    for ship in db.incoming_ships:
-        print(f"Ship {ship['ship_name']} - Size: {ship['size']}, Arrival: {ship['arrival_time']}, Departure: {ship['departure_time']}")
+# I also figured out itertools, importing, and dictionaries from GeeksforGeeks.
 
 # Main function
 def main():
@@ -25,11 +15,6 @@ def main():
 
 # Function to update the schedule to accomodate the incoming ships
 def updating_schedule():
-    """for ship in db.incoming_ships:
-        bay = available(ship) - 1 
-        detail = (ship['arrival_time'], ship['departure_time'], ship['ship_name'])
-        db.docking_bays[bay]['schedule'].append(detail)"""
-    
     max_bays = 0
     available_bay = []
     for ship in db.incoming_ships:
@@ -43,83 +28,66 @@ def updating_schedule():
         possibilities.append(num)
     paths = list(itertools.product(possibilities, repeat=len(db.incoming_ships)))
 
-    fake_schedule = 
+    first_schedule = []
+    for bay in db.docking_bays:
+        first_schedule.append(bay['schedule'])
 
+    times_between = {}
     for path in paths:
-        for num, bay in path:
+        can_do = True
+        for num, bay in enumerate(path):
+            try:
+                bay_index = available_bay[num][bay]
+                ship = db.incoming_ships[num]
+                if available_time(db.docking_bays[bay_index], ship):
+                    detail = (ship['arrival_time'], ship['departure_time'], ship['ship_name'])
+                    db.docking_bays[bay_index]['schedule'].append(detail)      
+                else:
+                    can_do = False   
+            except IndexError:
+                can_do = False
+        if can_do:
+            organize_schedule()
+            times_between.update({find_time_excess(): path})
+        for num, bay in enumerate(db.docking_bays):
+            bay['schedule'] = first_schedule[num]
 
+        times_between = OrderedDict(sorted(times_between.items()))
+        print(times_between)
+        """first_key = list(times_between.keys())[0]
+        best_path = times_between[first_key]
 
-    
+        for num, bay in enumerate(best_path):
+            bay_index = available_bay[num][bay]
+            ship = db.incoming_ships[num]
+            detail = (ship['arrival_time'], ship['departure_time'], ship['ship_name'])
+            db.docking_bays[bay_index]['schedule'].append(detail)"""
 
         
+def find_time_excess():
+    count = 0
+    for bay in db.docking_bays:
+        for num, the_ship in enumerate(bay['schedule']):
+            first = get_time(the_ship[1])
+            try:
+                second = get_time(bay['schedule'][num+1][0])
+            except:
+                break
+            count = count + second - first
+        if bay['schedule'] == []:
+            count = count + 24
 
-# Function to check the bays available for the ships based on size and time and return it.
+    return count
+
+# Function to check the bays available for the ships based on size and return it.
 ## Note: I did it small to small, medium to medium, and large to large.
 def available_size(ship):
     filtered_size = []
     for bay in db.docking_bays:
         if bay['size'] == ship['size']:
-            filtered_size.append(bay)
+            filtered_size.append(bay['bay_id']-1)
     return filtered_size
     
-def available_time(bay):
-    filtered_time = []
-    new_arrival = get_time(ship['arrival_time'])
-    new_depart = get_time(ship['departure_time'])
-
-    can = True
-    for schedule in bay['schedule']:
-        current_arrival = get_time(schedule[0])
-        current_depart = get_time(schedule[1])
-        if new_arrival < current_depart and current_arrival < new_depart:
-            can = False
-    return can
-
-# Function to turn time into decimal and return it. Ex. 12:30 = 12.3
-def get_time(s):
-    index = 0
-    for num, letter in enumerate(s):
-        if letter == ':':
-            index = num
-
-    s = s[:index] + '.' + s[index+1:]
-    s = float(s)
-    return s
-
-
-# Function to turn decimal into time and return it. Ex. 12.3 = 12:30
-def turn_back_time(s, old):
-    index = 0
-    s = str(s)
-    for num, letter in enumerate(s):
-        if letter == '.':
-            index = num
-
-    s = s[:index] + ':' + s[index+1:]
-    if old[4] == '0':
-        s = s + '0'
-    return s
-
-# Function to organize the schedule based on time
-def organize_schedule():
-    for bay in db.docking_bays:
-        result = []
-
-        for num in range(len(bay['schedule'])):
-            added_ship = bay['schedule'][0]
-            min = get_time(added_ship[0])
-            
-            for ship in bay['schedule']:
-                if get_time(ship[0]) < min:
-                    min = get_time(ship[0])
-                    added_ship = ship
-
-            result.append(added_ship)
-            bay['schedule'].remove(added_ship)
-
-        bay['schedule'] = result
-
-
 # Function to print the new schedule in a human-readable format.
 def print_schedule():
     print("\nSchedule")
@@ -157,6 +125,65 @@ def print_schedule():
 
             print(f"{ship[2]} - {arrival} {arr_period} to {depart} {dep_period}", end = ending)
         print()
+
+# Function to turn time into decimal and return it. Ex. 12:30 = 12.3
+def get_time(s):
+    index = 0
+    s = str(s)
+    for num, letter in enumerate(s):
+        if letter == ':':
+            index = num
+
+    s = s[:index] + '.' + s[index+1:]
+    s = float(s)
+    return s
+
+
+# Function to turn decimal into time and return it. Ex. 12.3 = 12:30
+def turn_back_time(s, old):
+    index = 0
+    s = str(s)
+    for num, letter in enumerate(s):
+        if letter == '.':
+            index = num
+
+    s = s[:index] + ':' + s[index+1:]
+    if old[4] == '0':
+        s = s + '0'
+    return s
+
+# Function to check if it is able to add to schedule without overlapping.
+def available_time(bay, ship):
+    filtered_time = []
+    new_arrival = get_time(ship['arrival_time'])
+    new_depart = get_time(ship['departure_time'])
+
+    can = True
+    for schedule in bay['schedule']:
+        current_arrival = get_time(schedule[0])
+        current_depart = get_time(schedule[1])
+        if (new_arrival < current_depart and new_arrival > current_arrival) or (new_depart > current_arrival and new_depart < current_depart):
+            can = False
+    return can
+
+# Function to organize the schedule based on time
+def organize_schedule():
+    for bay in db.docking_bays:
+        result = []
+
+        for num in range(len(bay['schedule'])):
+            added_ship = bay['schedule'][0]
+            min = get_time(added_ship[0])
+            
+            for ship in bay['schedule']:
+                if get_time(ship[0]) < min:
+                    min = get_time(ship[0])
+                    added_ship = ship
+
+            result.append(added_ship)
+            bay['schedule'].remove(added_ship)
+
+        bay['schedule'] = result
 
 if __name__ == "__main__":
     main()
---
+++ starterProject.py
@@ -0,0 +1,12 @@
+import dockingBays as db
+# Function to print docking bays information
+def print_docking_bays():
+    print("Docking Bays")
+    for bay in db.docking_bays:
+        print(f"Bay {bay['bay_id']} - Size: {bay['size']}, Schedule: {bay['schedule']}")
+
+# Function to print incoming ships information
+def print_incoming_ships():
+    print("\nIncoming Ships")
+    for ship in db.incoming_ships:
+        print(f"Ship {ship['ship_name']} - Size: {ship['size']}, Arrival: {ship['arrival_time']}, Departure: {ship['departure_time']}")
